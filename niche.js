b917ae7eefcead130dd605b708d8f3f14a265836
Suhaas <suhaas69@gmail.com>
2022-03-21 20:13:34 +0530

    compose pipe memoize custom js functions

---
+++ niche.js
@@ -0,0 +1,69 @@
+//compose
+
+function compose(...arrayOfFunctions) {
+  return function (...args) {
+    let res = Number(args);
+    for (let i = arrayOfFunctions.length - 1; i >= 0; i--) {
+      const currentFn = arrayOfFunctions[i];
+      res = currentFn(res);
+    }
+    return res;
+  };
+}
+
+function pipe(...arrayOfFunctions) {
+  return function (...args) {
+    let res = Number(args);
+    for (let i = 0; i < arrayOfFunctions.length; i++) {
+      const currentFn = arrayOfFunctions[i];
+      res = currentFn(res);
+    }
+    return res;
+  };
+}
+
+function addTen(number) {
+  return number + 10;
+}
+
+function square(number) {
+  return number * number;
+}
+
+let composedFn = compose(square, addTen);
+
+console.log(composedFn(30));
+
+//pipe
+
+//memoize
+
+const memoize = (fn) => {
+  let cache = {};
+  return (...args) => {
+    let n = args[0]; // just taking one argument here
+    if (n in cache) {
+      console.log("Fetching from cache");
+      return cache[n];
+    } else {
+      console.log("Calculating result");
+      let result = fn(n);
+      cache[n] = result;
+      return result;
+    }
+  };
+};
+
+const memoize2 = (func) => {
+  const cache = {};
+  return (...args) => {
+    console.log(cache);
+    let strKey = args.join(",");
+    if (!cache[strKey]) {
+      console.log("adding to cache!");
+      cache[strKey] = func.apply(this, args);
+    }
+    console.log("fetching from cache!");
+    return cache[strKey];
+  };
+};
