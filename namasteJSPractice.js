79274e648271187c2cd4067e2d45af0d2634e7be
Suhaas <suhaas69@gmail.com>
2023-06-15 12:58:33 +0530

    namasteJS practise

---
+++ namasteJSPractice.js
@@ -0,0 +1,20 @@
+// var x = 7;
+// const getName = () => {
+//   console.log("namaste javascript");
+// };
+
+// getName();
+// console.log(x);
+// console.log(getName);
+
+function test() {
+  let z = 1000;
+  function y() {
+    let x = 100;
+    console.log(z);
+  }
+  y();
+  console.log(x);
+}
+
+test();
